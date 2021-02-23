abfbdcd7dd9b326102a10f94e375509bb1dbb1cd
Théo Percheron <77283423+Tpercher@users.noreply.github.com>
2021-02-23 17:00:50 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title> Site professionnel de Theo Percheron </title>
+		<meta name="description" content="description à inscrire ici" />
+		<meta name="google-site-verification" content="3I7gOh7jw9or_G40-4XOEO6GEYsEV0ZG-ZbfzA84PtM" />
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/style.css">
+		<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300" />
+		<link rel="canonical" href="https://theopercheron.com/" />
+	</head>
+	<body>
+		<header>
+			<nav class="navigation">
+				<img class="logo" src="img/Logo_Full_Blanc.png" alt="Mon logo" title="Mon logo professionnel">
+				<div class="liens">
+					<a href="#biographie" title="Acceuil">Présentation</a>
+					<a href="#competences" title="Projet_iim">Compétences</a>
+					<a href="#realisations" title="Projet_perso">Mes réalisations</a>
+					<a href="#contact" title="Avis">Contact </a>
+				</div>
+			</nav>
+			<h1 class="animation-texte">Théo Percheron<br />
+			Étudiant à l'<span class="couleuriim">IIM</span><br />
+			Est un
+			  <span class="txt-rotate" data-period="200" data-rotate='[ "Marketeur", "Développeur", "Auto-entrepreneur" ]'></span>
+			</h1>
+   </header>
+<footer>
+		<a href="index.php" title="mon site">Theo Percheron</a>
+		<span> | &copy; 2017 | </span>
+		<a href="#" title="mentions légales"> Tout droit réservé </a>
+	</footer>
+	</body>
+	<script src="script.js"></script>
+</html>
